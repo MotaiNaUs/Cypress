1cc595e5aad7b0abe37e2aa0cba7f378171aac21
MotaiNaUs <Suaniatom1@yandex.ru>
2023-10-22 07:53:13 +0300

    sync

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,19 @@
 	const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: 'junit',
+  reporter: 'cypress-mochawesome-reporter',
   reporterOptions: {
-    mochaFile: 'results/my-test-output.xml',
-    toConsole: true,
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    embeddedScreenshots:true,
   },
   e2e: {
+    video:false,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
 		// on('before:browser:launch', (browser, launchOptions) => {
     //   console.log(launchOptions.preferences);
     //   launchOptions.preferences.browser.isHeadless = false;
